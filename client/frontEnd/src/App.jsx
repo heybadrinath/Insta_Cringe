8210f1533760381fbd566ce1fef20817c713e944
Badri <ram.r@kalvium.community>
2024-02-23 12:25:23 +0530

    vite add

---
+++ client/frontEnd/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="home">
+      <h1>Welcome To Insta Cringe</h1>
+      <p>
+        Discover the ultimate collection of cringe-worthy Instagram bios all in
+        one place. From the hilariously awkward to the downright bizarre, we've
+        got it covered. Scroll through our curated list and prepare to laugh,
+        cringe, and maybe even find some inspiration for your own bio. Join us
+        in celebrating the quirks and eccentricities of Instagram bios on
+        InstaCringeBio!
+      </p>
+    </div>
+  );
+}
+
+export default App;
