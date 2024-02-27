cb8f7b66e433a860d666d94aedfbbc6f13e783c3
Badri <ram.r@kalvium.community>
2024-02-27 17:13:16 +0530

    commit dtaa

--- client/frontEnd/src/App.jsx
+++ client/frontEnd/src/App.jsx
@@ -9,8 +9,8 @@ function App() {
     fetchData();
   }, []);
   const fetchData = async () => {
-    const apiLink = "https://badri-squad51-insta-cringe.onrender.com/getBio";
-    // const apiLink = "http://localhost:3000/getBio";
+    // const apiLink = "https://badri-squad51-insta-cringe.onrender.com/getBio";
+    const apiLink = "http://localhost:3000/getBio";
     try {
       const response = await fetch(apiLink);
       const responseData = await response.json();
