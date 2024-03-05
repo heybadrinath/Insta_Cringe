91ca33473c442f52d0935e0d95b772fbda7487b0
Badri <ram.r@kalvium.community>
2024-03-05 11:58:26 +0530

    cookies endpoint added

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const port = "3000";
 const { startDB, dbStatus } = require("./database");
 const routes = require("./routes/routes");
 const cors = require("cors")
+const cookieParser = require('cookie-parser');
+
 app.use(express.json());
 app.use(cors())
 app.use("/", routes);
@@ -15,6 +17,20 @@ app.get("/", (req, res) => {
 app.get("/ping", (req, res) => {
   res.send("pong");
 });
+
+
+app.use(cookieParser());
+app.post('/login', (req, res) => {
+    const { userName } = req.body;
+    res.cookie('userName', userName);
+    res.send('Login successful');
+});
+
+app.get('/logout', (req, res) => {
+    res.clearCookie('userName');
+    res.send('Logout successful');
+});
+
 
 if (require.main === module) {
   app.listen(port, async () => {
